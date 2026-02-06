f1faa07b26f0f04d4b72c8d88e5b151d1f1783c6
gdramaneouattara <gdramane.ouattara@gmail.com>
2026-02-06 16:37:34 +0000

    Fix URL polyfill import for EAS

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,12 @@
 
-import 'react-native-url-polyfill/auto'
+import { Platform } from 'react-native'
+import { setupURLPolyfill } from 'react-native-url-polyfill'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+
+if (Platform.OS !== 'web') {
+  setupURLPolyfill()
+}
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
