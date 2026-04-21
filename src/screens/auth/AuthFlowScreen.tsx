f30ac9dc37c8febeef9eb5b815d8fa1a7b4a13f3
gdramaneouattara <gdramane.ouattara@gmail.com>
2026-04-21 02:07:12 +0000

    feat: update legal document URLs to Google Sites

--- src/screens/auth/AuthFlowScreen.tsx
+++ src/screens/auth/AuthFlowScreen.tsx
@@ -29,10 +29,10 @@ import { uploadArrayBufferToBucket } from '../../lib/storageUpload';
 
 const TERMS_URL =
   process.env.EXPO_PUBLIC_TERMS_URL ||
-  'https://https://sites.google.com/view/yamo-app-legal/cgu';
+  'https://sites.google.com/view/yamo-app-legal/cgu';
 const PRIVACY_URL =
   process.env.EXPO_PUBLIC_PRIVACY_URL ||
-  'https://https://sites.google.com/view/yamo-app-legal/accueil';
+  'https://sites.google.com/view/yamo-app-legal/accueil';
 
 const INTERESTS_OPTIONS = [
   'Voyage',
